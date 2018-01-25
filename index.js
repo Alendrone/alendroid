e09e85478b664da413d6238484888e6a56813f9e
Alendrone <33103761+Alendrone@users.noreply.github.com>
2018-01-25 13:50:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ client.on("message", async msg => {
   var args = msg.content.substring(entity.length).trim().split(/ +/g);
   if (msg.toString().startsWith(entity)) {
     if (args == "ping") {
-      var ping = Math.round(client.ping);
-      msg.channel.send(`Ping is ${ping}ms`);
+      var pulse = Math.round(client.ping);
+      msg.channel.send(`Ping is ${pulse}ms`);
     }
   }
 });
