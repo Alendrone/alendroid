9d8a29ee4313c2e78abe9d628bc728f79d8a71a2
Alendrone <33103761+Alendrone@users.noreply.github.com>
2018-01-25 13:21:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var entity = "<@382115660336988161>";
 
-client.on("message", message => {
-
-  var args = message.content.substring(entity.length).trim().split(/ +/g);
-
-  if (message.toString().startsWith(entity) && !message.toString().endsWith(entity)) {
-
-    message.reply("Working!");
-
+client.on("message", async msg => {
+  var args = msg.content.substring(entity.length).trim().split(/ +/g);
+  if (msg.toString().starts With(entity)) {
+    if (args == "ping") {
+      msg.channel.send(`Ping is *${client.ping}ms*`);
+    }
   }
-
-  if (!message.toString().startsWith(entity) && message.toString().endsWith(entity)) {
-
-    message.reply("Working!");
-
-  }
-
 });
 
 
